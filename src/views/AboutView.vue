b7626bb79ea47e48b3e5027e48feb50a5e60cee0
gldaniil <daniil.gajfullin@yandex.ru>
2024-04-30 22:41:18 +0500

    Добавляю библиотеку с иконками, первые наброски для основных страниц; исправляю структуру навигации

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h2>About View Page</h2>
+  <the-title>О программе</the-title>
+  <div class="border-2 border-blue-300 rounded-xl bg-white mt-5 h-full">
+    <div class="text-lg tracking-wider p-4">
+      <p>Привет!!!!!!!</p>
+    </div>
   </div>
 </template>
+<script setup>
+import TheTitle from '../components/TheTitle.vue'
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,12 @@
 <template>
-  <h2>Home View Page</h2>
+  <the-title>Главная</the-title>
+  <div class="border-2 border-blue-300 rounded-xl bg-white mt-5 h-full">
+    <div class="text-lg tracking-wider p-4">
+      <p>Привет!!!!!!!</p>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import TheTitle from '../components/TheTitle.vue'
+</script>
