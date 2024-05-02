0b2ab41d91c398a845aaf8a2f30077bacaee525e
gldaniil <daniil.gajfullin@yandex.ru>
2024-05-02 21:17:07 +0500

    Исправляю стили компонентов

--- client/src/components/TheTitle.vue
+++ client/src/components/TheTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border-2 border-blue-300 rounded-xl bg-white">
+  <div class="border-1 shadow-xl rounded-xl bg-white">
     <h2 class="text-3xl font-medium text-black p-4">
       <slot></slot>
     </h2>
