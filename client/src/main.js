94999804e562f129211359744d787b018e666285
gldaniil <daniil.gajfullin@yandex.ru>
2024-05-04 00:19:04 +0500

    Добавляю новую иконку для страницы "Редактирование"

--- client/src/main.js
+++ client/src/main.js
@@ -3,17 +3,23 @@ import './assets/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { CoHome, BiQuestionCircle, MdArrowforwardios } from 'oh-vue-icons/icons'
+import {
+  CoHome,
+  BiQuestionCircle,
+  MdArrowforwardios,
+  BiPlusCircle
+} from 'oh-vue-icons/icons'
 
 import App from './App.vue'
 import router from './router'
 
-addIcons(CoHome, BiQuestionCircle, MdArrowforwardios)
+addIcons(CoHome, BiQuestionCircle, MdArrowforwardios, BiPlusCircle)
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.component('v-icon', OhVueIcon)
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
