e59db3f42b24d88fc0da450cdec29eefb3e49303
gldaniil <daniil.gajfullin@yandex.ru>
2024-06-24 22:27:35 +0500

    client: Добавляю запрос на сервер [список доступных ссылок]

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,11 +4,29 @@
     <div class="shadow-xl rounded-xl bg-black/30 mt-5 h-full">
       <div class="text-lg tracking-wider p-4">
         <p class="text-white">Lorem ipsum dolor sit amet.</p>
+        <button @click="getCourts" class="text-white">Получить суды</button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { inject, ref } from 'vue'
 import TheTitle from '../components/TheTitle.vue'
+
+const axios = inject('axios')
+const courts = ref([])
+
+async function getCourts() {
+  const { data } = await axios.get('/api/courts')
+  console.log(data)
+  courts.value = data
+}
+// FOR RELEASE
+// onMounted(() => {
+//   async function getCourts() {
+//     const { data } = await axios.get('/api/courts')
+//   }
+//   getCourts()
+// })
 </script>
