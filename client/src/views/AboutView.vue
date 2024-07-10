8b0c2d49a1c68915b5d0ceb1b61ede9a34028c5b
Dan <gldaniil.zlt@gmail.com>
2024-07-10 09:19:23 +0500

    Добавляю корректное отображение высоты блоков

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="h-full flex flex-col pl-8 pr-6 py-6">
+  <div class="flex flex-col pl-8 pr-6 py-6 h-full">
     <the-title>О программе</the-title>
-    <div class="shadow-xl rounded-xl bg-black/30 mt-5 h-full">
+    <div class="shadow-xl rounded-xl bg-black/30 mt-5 h-full overflow-auto">
       <div class="text-lg tracking-wider p-4">
         <p class="text-white">Lorem ipsum dolor sit amet.</p>
       </div>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-full flex flex-col pl-8 pr-6 py-6">
     <the-title>Главная</the-title>
-    <div class="shadow-xl rounded-xl bg-black/30 mt-5 h-full">
+    <div class="shadow-xl rounded-xl bg-black/30 mt-5 h-full overflow-auto">
       <div class="text-lg tracking-wider p-4">
         <div v-if="!courts.length" class="text-white">
           <p>Список доступных городов пуст.</p>
